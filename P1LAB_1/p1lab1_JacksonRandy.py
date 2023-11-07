14840e1792be0586abe1436394cb0adc4f161977
xtremiss <68485250+xtremiss@users.noreply.github.com>
2023-11-07 11:23:50 -0500

    Add files via upload

---
+++ P1LAB_1/p1lab1_JacksonRandy.py
@@ -0,0 +1,9 @@
+# Randy Jackson
+# 11/7/2023
+# Basic Input/Output
+
+# Get input from user
+name = input("please enter your name: ")
+
+#Display outpout to user 
+print("Hello", name, "Welcome to CTI-110!")
